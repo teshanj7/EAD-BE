db427757922de68a2751f0fa79faf97a63a3f880
ManulaGunatilleke <inner.gunatilleke@gmail.com>
2024-09-28 14:07:19 +0530

    OrderManagement Related Updates

---
+++ EADEcommerceBE/Models/Order.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace OrderManagement.Model
+{
+    public class Order
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+        
+        public string UserId { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        public double TotalPrice { get; set; }
+        
+        public string DeliveryStatus { get; set; }  // Processing, Shipped, Delivered, etc.
+        
+        public string OrderStatus { get; set; }  // Approve or Cancel
+        
+        public string OrderNumber { get; set; }
+        
+        public bool IsCancel { get; set; }  // If true, provide a cancellation note
+        
+        public string CancellationNote { get; set; }  // Reason for cancellation
+        
+        public DateTime OrderDate { get; set; }
+    }
+
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
