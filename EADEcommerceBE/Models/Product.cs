ef5c6bb343fc76925ce6bdd0d970f399190bcfa7
Teshan Jayakody <teshanjayakody217@gmail.com>
2024-09-28 11:52:18 +0530

    Add project files.

---
+++ EADEcommerceBE/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace EadECommerce.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+
+        public ObjectId Id { get; set; }
+        public string? ProductName { get; set; }
+        public string? ProductCategory { get; set; }
+        public string? ProductDescription { get; set; }
+        public int ProductQuantity { get; set; }
+        public string? ProductVendor { get; set; }
+        public bool ProductStatus { get; set; }
+        public bool ProductAvailability { get; set; }
+        public string? ProductImage { get; set; }
+        public int ProductPrice { get; set; }
+    }
+}
---
+++ EADEcommerceBE/Program.cs
@@ -0,0 +1,36 @@
+using MongoDB.Driver;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var mongoclient = new MongoClient(configuration.GetConnectionString("MongoDb"));
+builder.Services.AddSingleton<MongoClient>(mongoclient);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
